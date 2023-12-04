bce3fee60f246a8821c75b76be548ad37f4e3ade
kikaio <gjduddnr5923@naver.com>
2023-12-04 21:01:26 +0900

    기본 작업 진행 중...

---
+++ insta/src/main/java/com/clone/insta/controllers/BasicController.java
@@ -0,0 +1,15 @@
+package com.clone.insta.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class BasicController {
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
