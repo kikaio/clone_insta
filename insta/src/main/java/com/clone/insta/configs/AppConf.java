69ed843aa1d9b28153456a1de5694606ad4fc570
kikaio <gjduddnr5923@naver.com>
2023-12-26 22:29:18 +0900

    oauth 관련 작업 중

--- insta/src/main/java/com/clone/insta/configs/AppConf.java
+++ insta/src/main/java/com/clone/insta/configs/AppConf.java
@@ -2,39 +2,55 @@ package com.clone.insta.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class AppConf {
 
+    static final String signInUrl = "/sign-in";
+    static final String entryUrl = "/";
+    static final String[] publicUlrls = {
+            entryUrl
+            , "/favicon.ico"
+            , "images/*"
+    };
+    static final String[] signUrls = {
+        signInUrl
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        String[] publicUlrls = {
-            "/"
-            , "/favicon.ico"
-            , "images/*"
-        };
-        String[] signUrls = {
-            "/sign-in"
-        };
         
          http
             .cors(cors->cors.disable())
             .csrf(csrf->csrf.disable())
             .authorizeHttpRequests(custom->{
-                custom
-                    .requestMatchers(publicUlrls).permitAll()
-                    .requestMatchers(signUrls).permitAll()
-                    .anyRequest().authenticated()
-                ;
+                try {
+                    custom
+                        .requestMatchers(publicUlrls).permitAll()
+                        .requestMatchers(signUrls).permitAll()
+                        .anyRequest().authenticated()
+                    ;                    
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             })
-            .formLogin(custom->{
-                custom.loginPage("/sign-in")
-                ;
+            .exceptionHandling(custom->{
+                custom.authenticationEntryPoint(
+                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                );
+            })
+            .logout(custom->{
+                custom.logoutSuccessUrl("/main");
+            })
+            .oauth2Login(custom->{
+                custom.loginPage(signInUrl);
             })
             ;
         return http.build();
