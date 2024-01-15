c5b641044efae513d411227108b5b05aeb6699e3
kikaio <gjduddnr5923@naver.com>
2024-01-15 19:46:57 +0900

    유저 계정 관련 entity 작성 및  기본적인 time용 entity cnrk.

---
+++ insta/src/main/java/com/clone/insta/Entities/Base/UTCTimeBaseEntity.java
@@ -0,0 +1,43 @@
+package com.clone.insta.Entities.Base;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@MappedSuperclass
+@Getter
+@ToString
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UTCTimeBaseEntity {
+
+    @Column
+    private OffsetDateTime cdate;
+
+    @Column
+    private OffsetDateTime mdate;
+
+    @PrePersist
+    public void prePersist()
+    {
+        this.cdate = OffsetDateTime.now(ZoneId.of("UTC"));
+        this.mdate = this.cdate;
+    }
+
+    @PreUpdate
+    public void preUpdate()
+    {
+        this.mdate = OffsetDateTime.now(ZoneId.of("UTC"));
+    }
+}
--- insta/src/main/java/com/clone/insta/Entities/InstaUser.java
+++ insta/src/main/java/com/clone/insta/Entities/InstaUser.java
@@ -1,31 +1,68 @@
 package com.clone.insta.Entities;
 
+import java.util.Collection;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.clone.insta.Entities.Base.UTCTimeBaseEntity;
 import com.clone.insta.enums.ERoles;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @SuperBuilder
-public class InstaUser {
+public class InstaUser extends UTCTimeBaseEntity implements UserDetails {
+
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
-    private String name;
-
+    @Column(nullable = false)
     private String email;
 
+    @Column(nullable = false)
+    @ColumnDefault("")
+    @Builder.Default
+    private String password = "";
+
+    @Column
+    private String signProvider;
+
+    @Column
     private ERoles role;
+
+    @Column
+    @ColumnDefault("false")
+    @Builder.Default
+    private boolean isAccountExpired = false;
+
+    @Column
+    @ColumnDefault("true")
+    @Builder.Default
+    private boolean isAccountNonLocked = true;
+
+    @Column
+    @ColumnDefault("false")
+    @Builder.Default
+    private boolean isCredentialsNonExpired = false;
+
+    @Column
+    @ColumnDefault("true")
+    @Builder.Default
+    private boolean isEnabled = true;
 
     public String name()
     {
@@ -36,4 +73,47 @@ public class InstaUser {
     {
         return this.role.getName();
     }
+
+    @Override
+	public String getPassword()
+    {
+        return this.password;
+    }
+
+    @Override
+	public String getUsername()
+    {
+        return this.email;
+    }
+
+    @Override
+	public boolean isAccountNonExpired()
+    {
+        return this.isAccountExpired;
+    }
+
+    @Override
+	public boolean isAccountNonLocked()
+    {
+        return this.isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return this.isCredentialsNonExpired;
+    }
+
+    @Override
+	public boolean isEnabled()
+    {
+        return this.isEnabled;
+    }
+
+    @Override
+    public Collection<SimpleGrantedAuthority> getAuthorities()
+    {
+        return null;
+    }
+
 }
