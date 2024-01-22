cb3a7eb34a5524d07be487a4c929cf73eb2fe50b
DESKTOP-NV48I2Q\user <dd>
2024-01-23 01:22:17 +0900

    redis template에서 에러 남던 부분 수정 ( StringRedisTemplate로 변경 )

--- insta/src/main/java/com/clone/insta/configs/RedisConf.java
+++ insta/src/main/java/com/clone/insta/configs/RedisConf.java
@@ -5,12 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConf {
-    
+
     @Value("${spring.redis.host}")
     private String host;
 
@@ -23,21 +23,22 @@ public class RedisConf {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+    public StringRedisTemplate stringRedisTemplate() {
+        var stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
 
         // 일반적인 key:value의 경우 시리얼라이저
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
 
         // Hash를 사용할 경우 시리얼라이저
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setHashValueSerializer(new StringRedisSerializer());
 
         // 모든 경우
-        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        stringRedisTemplate.afterPropertiesSet();
 
-        return redisTemplate;
+        return stringRedisTemplate;
     }
 }
