ad0f03f0c8c1e1000d248712307db91c851ac08e
DESKTOP-NV48I2Q\user <dd>
2024-01-23 01:04:17 +0900

    권한 허가 url pattern 추가

--- insta/src/main/java/com/clone/insta/configs/AppConf.java
+++ insta/src/main/java/com/clone/insta/configs/AppConf.java
@@ -19,59 +19,46 @@ public class AppConf {
     static final String signInUrl = "/sign-in";
     static final String entryUrl = "/";
     static final String mainPageUrl = "/main";
+    static final String faviconUrl = "/favicon.ico";
+    static final String imagesUrl = "/images/**";
     static final String[] publicUlrls = {
-            entryUrl
+            entryUrl, faviconUrl, imagesUrl
     };
     static final String[] signUrls = {
-        signInUrl
+            signInUrl
     };
 
     private final CustomOAuthService customOAuthService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
-        
-         http
-            .cors(cors->cors.disable())
-            .csrf(csrf->csrf.disable())
-            .authorizeHttpRequests(custom->{
-                try {
-                    custom
-                        .requestMatchers(publicUlrls).permitAll()
-                        .requestMatchers(signUrls).permitAll()
-                        .anyRequest().authenticated()
-                    ;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            })
-            .logout(custom->{
-                custom.logoutSuccessUrl(mainPageUrl);
-            })
-            .formLogin(custom->{
-                custom.loginPage(signInUrl);
-            })
-            .oauth2Login(custom->{
-                custom.loginPage(signInUrl);
-                custom.userInfoEndpoint(conf->{
-                    conf.userService(customOAuthService);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(custom -> {
+                    try {
+                        custom
+                                .requestMatchers(publicUlrls).permitAll()
+                                .requestMatchers(signUrls).permitAll()
+                                .anyRequest().authenticated();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                })
+                .logout(custom -> {
+                    custom.logoutSuccessUrl(mainPageUrl);
+                })
+                .formLogin(custom -> {
+                    custom.loginPage(signInUrl);
+                })
+                .oauth2Login(custom -> {
+                    custom.loginPage(signInUrl);
+                    custom.userInfoEndpoint(conf -> {
+                        conf.userService(customOAuthService);
+                    });
                 });
-            })
-            ;
         return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer()
-    {
-        final String[] ignorePath = {
-            "/static/favicon.ico"
-            , "/static/images/**"
-        };
-
-        return (web)->{
-            web.ignoring().requestMatchers(ignorePath);
-        };
-    } 
 }
